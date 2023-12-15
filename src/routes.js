6cfcd4f5b343f4edd51a50616ce0b5e6c42783bd
Tunahan Çakıl <tunahancakil@gmail.com>
2023-12-16 02:46:54 +0300

    feat: change sidebar

--- src/routes.js
+++ src/routes.js
@@ -37,13 +37,14 @@ Coded by www.creative-tim.com
 
 // Material Dashboard 2 React layouts
 import Dashboard from "layouts/dashboard";
-import Tables from "layouts/tables";
+import Coaches from "pages/coaches";
+import Courts from "pages/courts";
+import Students from "pages/students";
+import Payments from "pages/payments";
 import Billing from "layouts/billing";
-import RTL from "layouts/rtl";
 import Notifications from "layouts/notifications";
 import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
-import SignUp from "layouts/authentication/sign-up";
 
 // @mui icons
 import Icon from "@mui/material/Icon";
@@ -59,11 +60,35 @@ const routes = [
   },
   {
     type: "collapse",
-    name: "Tables",
-    key: "tables",
+    name: "Antrenörler",
+    key: "coahces",
     icon: <Icon fontSize="small">table_view</Icon>,
-    route: "/tables",
-    component: <Tables />,
+    route: "/coahces",
+    component: <Coaches />,
+  },
+  {
+    type: "collapse",
+    name: "Kortlar",
+    key: "courts",
+    icon: <Icon fontSize="small">table_view</Icon>,
+    route: "/courts",
+    component: <Courts />,
+  },
+  {
+    type: "collapse",
+    name: "Öğrenciler",
+    key: "students",
+    icon: <Icon fontSize="small">table_view</Icon>,
+    route: "/students",
+    component: <Students />,
+  },
+  {
+    type: "collapse",
+    name: "Ödemeler",
+    key: "payments",
+    icon: <Icon fontSize="small">table_view</Icon>,
+    route: "/payments",
+    component: <Payments />,
   },
   {
     type: "collapse",
@@ -75,43 +100,11 @@ const routes = [
   },
   {
     type: "collapse",
-    name: "RTL",
-    key: "rtl",
-    icon: <Icon fontSize="small">format_textdirection_r_to_l</Icon>,
-    route: "/rtl",
-    component: <RTL />,
-  },
-  {
-    type: "collapse",
     name: "Notifications",
     key: "notifications",
     icon: <Icon fontSize="small">notifications</Icon>,
     route: "/notifications",
     component: <Notifications />,
-  },
-  {
-    type: "collapse",
-    name: "Profile",
-    key: "profile",
-    icon: <Icon fontSize="small">person</Icon>,
-    route: "/profile",
-    component: <Profile />,
-  },
-  {
-    type: "collapse",
-    name: "Sign In",
-    key: "sign-in",
-    icon: <Icon fontSize="small">login</Icon>,
-    route: "/authentication/sign-in",
-    component: <SignIn />,
-  },
-  {
-    type: "collapse",
-    name: "Sign Up",
-    key: "sign-up",
-    icon: <Icon fontSize="small">assignment</Icon>,
-    route: "/authentication/sign-up",
-    component: <SignUp />,
   },
 ];
 
